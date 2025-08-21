6a61e313c02814cebc61a7b487e84da8c265c13e
AlexMartinez <informatica@xelapan.com>
2025-08-21 09:46:43 -0600

    Modulo Pago por lotes

--- dev_invoice_multi_payment/__manifest__.py
+++ dev_invoice_multi_payment/__manifest__.py
@@ -4,20 +4,21 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2015 DevIntelle Consulting Service Pvt.Ltd (<http://www.devintellecs.com>).
 #
-#    For Module Support : devintelle@gmail.com  or Skype : devintelle
+#    For Module Support : devintelle@gmail.com  or Skype : devintelle 
 #
 ##############################################################################
 {
-    "name": "Multiple Invoice Payment",
-    "version": "18.0.1.0",
-    "sequence": 1,
-    "description": """
-         App will allow multiple invoice payment from payment and invoice screen.
+    'name': 'Multiple Invoice Payment | Invoice Multi Payment | Invoice payments',
+    'version': '18.0.1.1',
+    'sequence':1,
+    "category": 'Accounting',
+    'description': """
+App will allow multiple invoice payment from payment and invoice screen.
         
-       Multiple invoice payment, Invoice Multiple payment, Payment , Partial Invoice Payment, Full invoice Payment,Payment write off,   Payment Invoice, 
-    Multiple invoice payment
-    Credit notes payment
-    How can create multiple invoice
+Multiple invoice payment, Invoice Multiple payment, Payment , Partial Invoice Payment, Full invoice Payment,Payment write off,   Payment Invoice, 
+Multiple invoice payment
+Credit notes payment
+How can create multiple invoice
 Invoice Management 
 Odoo Multiple Invoice Payment 
 Odoo Invoice Management 
@@ -78,30 +79,33 @@ Odoo Invoice Payment Journal Entry
 Manage Invoice Payment Journal Entry 
 Odoo Manage Invoice Payment Journal Entry 
 
-
     """,
-    "category": "Accounting",
-    "summary": "multiple Invoice payments apps use to easy payment multi invoice payment | multi-vendor bill payment mass invoice payment | mass bill payment | multiple invoice payment | multiple bill payment, multiple partial payment | multi-payment vendor bill | multiple payment vendor bills, multi invoice payment",
-    "author": "DevIntelle Consulting Service Pvt.Ltd",
-    "website": "http://www.devintellecs.com",
-    "depends": ["sale_management", "account"],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/account_payment.xml",
-        "wizard/bulk_invoice_payment.xml",
-    ],
-    "demo": [],
-    "test": [],
-    "css": [],
-    "qweb": [],
-    "js": [],
-    "images": ["images/main_screenshot.png"],
-    "installable": True,
-    "application": True,
-    "auto_install": False,
-    "price": 45.0,
-    "currency": "EUR",
+    'summary': 'multiple Invoice payments apps use to easy payment multi invoice payment multi-vendor bill payment mass invoice payment  mass bill payment   multiple invoice payment  multiple bill payment multiple partial payment  multi-payment vendor bill  multiple payment vendor bills multi invoice payment',      
+    'depends': ['sale_management','account'],
+    'data': [
+            'security/ir.model.access.csv',
+            'views/account_payment.xml',
+            'wizard/bulk_invoice_payment.xml',
+            ],
+	'demo': [],
+    'test': [],
+    'css': [],
+    'qweb': [],
+    'js': [],
+    'images': ['images/main_screenshot.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+    #author and support Details
+    'author': 'DevIntelle Consulting Service Pvt.Ltd',
+    'website': 'https://www.devintellecs.com',    
+    'maintainer': 'DevIntelle Consulting Service Pvt.Ltd', 
+    'support': 'devintelle@gmail.com',
+    'price':45.0,
+    'currency':'EUR',
     #'live_test_url':'https://youtu.be/A5kEBboAh_k',
-    "license": "LGPL-3",
+    "license":"LGPL-3",
+    "pre_init_hook" :"pre_init_check",
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
